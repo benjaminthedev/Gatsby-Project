f7f039f92235749397cd42c6dbcb56699d260cd6
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-11-30 07:02:25 +0000

    Start of project

---
+++ my-gatsby-project/src/pages/about.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+import SEO from "../components/seo"
+
+const AboutPage = () => (
+    <Layout>
+        <SEO
+            title="Home"
+            description="This is the about page!"
+            title="Hello World About Page"
+        />
+        <h2>This is the about page</h2>
+
+        <h1>Navigation:</h1>
+        <Link to="/page-2/">Page 2</Link> <br />
+        <Link to="/about/">About Me</Link> <br />
+        <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </Layout>
+)
+
+export default AboutPage
