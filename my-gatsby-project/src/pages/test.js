7b782db88dfd2c6d8aaa91deb02b9b7348731956
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-12-03 20:02:05 +0000

    Added test page

---
+++ my-gatsby-project/src/pages/test.js
@@ -0,0 +1,48 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import CodeImage from "../components/new"
+import SEO from "../components/seo"
+import NewImage from "../components/NewImage"
+
+
+
+
+
+
+const IndexPage = () => (
+    <Layout>
+        <SEO title="Test Page"
+        />
+        <h1>This is a Test Gastby Build</h1>
+
+        <p>Now go build something great.</p>
+        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+            <Image />
+        </div>
+
+        <div style={{ maxWidth: `900px`, marginBottom: `1.45rem` }}>
+            <CodeImage />
+        </div>
+
+
+        <NewImage
+            src={"/images/myEyes.png"}
+            alt={"Oh lord my eyes!"}
+        />
+
+        <NewImage
+            src={"/images/Code.png"}
+            alt={"New Code!"}
+        />
+
+
+
+        <Link to="/page-2/">Page 2</Link> <br />
+        <Link to="/about/">About Me</Link> <br />
+    </Layout>
+)
+
+export default IndexPage
